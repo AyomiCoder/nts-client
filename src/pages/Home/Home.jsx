7bf50560df9bab8e6cdd67915c57ca336bf128a6
AyomiCoder <alukostephen01@gmail.com>
2024-09-26 12:06:04 +0100

    login api integration

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,6 +4,7 @@ import NoteCard from '../../components/Card/NoteCard'
 import { MdAdd } from 'react-icons/md'
 import AddEditNotes from './AddEditNotes'
 import Modal from 'react-modal'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
 
@@ -12,6 +13,11 @@ const Home = () => {
     type: "add",
     data: null,
   })
+
+  const [userInfo, setUserInfo] = useState(null)
+
+  const navigate = useNavigate()
+
   return (
     <>
       <Navbar />
